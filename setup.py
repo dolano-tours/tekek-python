d069b22ef30ede78b44dbc79f941b77f61fa6320
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2020-09-25 20:34:03 +0700

    changed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tekek",
-    version="0.1.6",
+    version="0.1.7",
     author="Erlangga Ibrahim",
     author_email="erlanggaibr2@gmail.com",
     description="An Asynchronous Remote and Local Debugging Tools",
