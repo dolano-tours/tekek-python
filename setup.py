7d6aaa5b41b781bd9c58b4fd573a68b22bc8b1fc
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2020-09-25 20:38:08 +0700

    type in setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     python_requires='>=3.7',
     classifiers=[
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8"
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha",
